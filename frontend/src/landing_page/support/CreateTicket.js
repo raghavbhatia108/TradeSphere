2f8855c54fe0d64cef87e5e610565c97701da579
Raghav bhatia <bhatiaraghav70@gmail.com>
2026-05-03 17:17:19 +0530

    Project ready for deployment

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const linkStyle = { background: "none", border: "none", padding: 0, cursor: "pointer", textDecoration: "none", lineHeight: "2", color: "inherit", display: "inline" };
+
 const CreateTicket = () => {
   return (
     <div className="container">
@@ -7,88 +9,69 @@ const CreateTicket = () => {
       <div className="row mt-5">
         <div className="col">
           <h1 className="fs-5 mb-5">
-            <i class="fa-solid fa-circle-plus" style={{paddingRight:'10px'}}></i>Account Opening
+            <i className="fa-solid fa-circle-plus" style={{paddingRight:'10px'}}></i>Account Opening
           </h1>
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Online Account Registration</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Offline Account Registration</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Business Account Setup</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>International Account Registration</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Pricing & Fees</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>TradeSphere Premium Account</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Getting Started Guide</a>
-          <br />
+          <button type="button" style={linkStyle}>Online Account Registration</button><br />
+          <button type="button" style={linkStyle}>Offline Account Registration</button><br />
+          <button type="button" style={linkStyle}>Business Account Setup</button><br />
+          <button type="button" style={linkStyle}>International Account Registration</button><br />
+          <button type="button" style={linkStyle}>Pricing &amp; Fees</button><br />
+          <button type="button" style={linkStyle}>TradeSphere Premium Account</button><br />
+          <button type="button" style={linkStyle}>Getting Started Guide</button><br />
         </div>
         <div className="col">
           <h1 className="fs-5 mb-5">
-            <i class="fa-solid fa-user" style={{paddingRight:'10px'}}></i>Your TradeSphere Account
+            <i className="fa-solid fa-user" style={{paddingRight:'10px'}}></i>Your TradeSphere Account
           </h1>
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Login & Security</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Account Settings & Preferences</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Wallet & Bank Details</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Profile Management</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Transfer & Asset Management</a>
-          <br />
+          <button type="button" style={linkStyle}>Login &amp; Security</button><br />
+          <button type="button" style={linkStyle}>Account Settings &amp; Preferences</button><br />
+          <button type="button" style={linkStyle}>Wallet &amp; Bank Details</button><br />
+          <button type="button" style={linkStyle}>Profile Management</button><br />
+          <button type="button" style={linkStyle}>Transfer &amp; Asset Management</button><br />
         </div>
         <div className="col">
           <h1 className="fs-5 mb-5">
-            <i class="fa-solid fa-chart-column" style={{paddingRight:'10px'}}></i>Trading & Tools
+            <i className="fa-solid fa-chart-column" style={{paddingRight:'10px'}}></i>Trading &amp; Tools
           </h1>
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Margin/Leverage & Products</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>TradeSphere Terminal</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Trading FAQs</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Corporate Actions</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Risk Management Tools</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>API Documentation</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Advanced Analytics</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Market Insights</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Price Alerts & Automation</a><br />
+          <button type="button" style={linkStyle}>Margin/Leverage &amp; Products</button><br />
+          <button type="button" style={linkStyle}>TradeSphere Terminal</button><br />
+          <button type="button" style={linkStyle}>Trading FAQs</button><br />
+          <button type="button" style={linkStyle}>Corporate Actions</button><br />
+          <button type="button" style={linkStyle}>Risk Management Tools</button><br />
+          <button type="button" style={linkStyle}>API Documentation</button><br />
+          <button type="button" style={linkStyle}>Advanced Analytics</button><br />
+          <button type="button" style={linkStyle}>Market Insights</button><br />
+          <button type="button" style={linkStyle}>Price Alerts &amp; Automation</button><br />
         </div>
       </div>
 
       <div className="row mt-5">
         <div className="col">
           <h1 className="fs-5 mb-5">
-          <i class="fa-solid fa-credit-card" style={{paddingRight:'10px'}}></i>Wallet & Payments
+            <i className="fa-solid fa-credit-card" style={{paddingRight:'10px'}}></i>Wallet &amp; Payments
           </h1>
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Deposit Funds</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Withdraw Funds</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Payment Methods</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Bank Account Management</a>
+          <button type="button" style={linkStyle}>Deposit Funds</button><br />
+          <button type="button" style={linkStyle}>Withdraw Funds</button><br />
+          <button type="button" style={linkStyle}>Payment Methods</button><br />
+          <button type="button" style={linkStyle}>Bank Account Management</button><br />
         </div>
         <div className="col">
           <h1 className="fs-5 mb-5">
-          <i class="fa-solid fa-c" style={{paddingRight:'10px'}}></i>Portfolio Manager
+            <i className="fa-solid fa-c" style={{paddingRight:'10px'}}></i>Portfolio Manager
           </h1>
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Trade Reports</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Account Ledger</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Portfolio Analytics</a>
-          <br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Performance Tracking</a>
-          <br />
+          <button type="button" style={linkStyle}>Trade Reports</button><br />
+          <button type="button" style={linkStyle}>Account Ledger</button><br />
+          <button type="button" style={linkStyle}>Portfolio Analytics</button><br />
+          <button type="button" style={linkStyle}>Performance Tracking</button><br />
         </div>
         <div className="col">
           <h1 className="fs-5 mb-5">
-          <i class="fa-regular fa-circle" style={{paddingRight:'10px'}}></i>Investment Hub
+            <i className="fa-regular fa-circle" style={{paddingRight:'10px'}}></i>Investment Hub
           </h1>
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Fund Investing Basics</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>About Investment Hub</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Selecting & Investing in Funds</a><br />
-          <a href="" style={{textDecoration:"none" , lineHeight:'2'}}>Automatic Investment Plans</a><br />
+          <button type="button" style={linkStyle}>Fund Investing Basics</button><br />
+          <button type="button" style={linkStyle}>About Investment Hub</button><br />
+          <button type="button" style={linkStyle}>Selecting &amp; Investing in Funds</button><br />
+          <button type="button" style={linkStyle}>Automatic Investment Plans</button><br />
         </div>
       </div>
     </div>
